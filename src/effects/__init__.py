80afdbdf3f4c7e42266aff440777e05d76902f64
zane <czl970721@gmail.com>
2026-04-24 13:29:35 -0400

    feat: add block progress bar effect

--- src/effects/__init__.py
+++ src/effects/__init__.py
@@ -1,6 +1,8 @@
 from src.effects.base import Effect
 from src.effects.spinners import BrailleSpinner
+from src.effects.progress import BlockProgress
 
 EFFECTS: list[type[Effect]] = [
     BrailleSpinner,
+    BlockProgress,
 ]
