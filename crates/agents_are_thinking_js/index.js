53ba3d8f6b03870a46767b2b39db820a5a668f48
zane <czl970721@gmail.com>
2026-04-29 21:22:02 -0400

    npm package wasm binding

---
+++ crates/agents_are_thinking_js/index.js
@@ -0,0 +1,44 @@
+import {
+  BarBounce, BarSeeSaw, BarWave,
+  BrailleArrow, BrailleBounce, BrailleBreathe, BrailleCheckerboard,
+  BrailleCheckerboard2x2, BrailleDissolve, BrailleFire, BrailleHeartbeat,
+  BrailleMatrix, BrailleNoise, BrailleRain, BrailleRandom, BrailleRipple,
+  BrailleScanner, BrailleSpin, BrailleSpin2, BrailleWave, BrailleZigzag,
+  DotArrow, DotBounce, DotHeartbeat, DotPulse, DotWave,
+  ShadeBlink, ShadeBreathe, ShadeFire, ShadeLayers, ShadePinch,
+  ShadeRipple, ShadeScanner, ShadeSeeSaw, ShadeWave,
+  SquareArrow, SquareBlink, SquareFill, SquarePulse,
+  VBlockBounce, VBlockBreathe, VBlockCascade, VBlockPulse, VBlockRain,
+  VBlockRipple, VBlockScanner, VBlockTide, VBlockWave,
+  WIDTH,
+} from "./pkg/agents_are_thinking_js.js";
+
+export const EFFECTS = [
+  BrailleSpin, BrailleSpin2, BrailleWave, BrailleRandom,
+  BrailleBreathe, BrailleRipple, BrailleBounce, BrailleRain,
+  BrailleZigzag, BrailleDissolve, BrailleFire, BrailleNoise,
+  BrailleHeartbeat, BrailleArrow, BrailleScanner, BrailleMatrix,
+  BrailleCheckerboard, BrailleCheckerboard2x2,
+  ShadeWave, ShadeScanner, ShadeFire, ShadeRipple,
+  ShadeBreathe, ShadeSeeSaw, ShadeBlink, ShadeLayers, ShadePinch,
+  BarBounce, BarWave, BarSeeSaw,
+  VBlockWave, VBlockScanner, VBlockTide, VBlockBreathe,
+  VBlockBounce, VBlockPulse, VBlockRipple, VBlockRain, VBlockCascade,
+  SquarePulse, SquareFill, SquareBlink, SquareArrow,
+  DotWave, DotPulse, DotHeartbeat, DotArrow, DotBounce,
+];
+
+export {
+  BarBounce, BarSeeSaw, BarWave,
+  BrailleArrow, BrailleBounce, BrailleBreathe, BrailleCheckerboard,
+  BrailleCheckerboard2x2, BrailleDissolve, BrailleFire, BrailleHeartbeat,
+  BrailleMatrix, BrailleNoise, BrailleRain, BrailleRandom, BrailleRipple,
+  BrailleScanner, BrailleSpin, BrailleSpin2, BrailleWave, BrailleZigzag,
+  DotArrow, DotBounce, DotHeartbeat, DotPulse, DotWave,
+  ShadeBlink, ShadeBreathe, ShadeFire, ShadeLayers, ShadePinch,
+  ShadeRipple, ShadeScanner, ShadeSeeSaw, ShadeWave,
+  SquareArrow, SquareBlink, SquareFill, SquarePulse,
+  VBlockBounce, VBlockBreathe, VBlockCascade, VBlockPulse, VBlockRain,
+  VBlockRipple, VBlockScanner, VBlockTide, VBlockWave,
+  WIDTH,
+};
